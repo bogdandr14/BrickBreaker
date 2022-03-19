89758be2a9ad38086514305f051972e37ca23355
Bogdan Draghici <bogdandraghici99@gmail.com>
2022-03-19 16:26:05 +0200

    added base game controlled via buttons

--- BrickBreakerGame/BrickBreakerForm.Designer.cs
+++ BrickBreakerGame/BrickBreakerForm.Designer.cs
@@ -29,22 +29,113 @@ namespace BrickBreakerGame
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BrickBreakerForm));
+            this.picPaddle = new System.Windows.Forms.PictureBox();
+            this.picBall = new System.Windows.Forms.PictureBox();
+            this.lblGameOver = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.lblStart = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picPaddle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).BeginInit();
             this.SuspendLayout();
+            // 
+            // picPaddle
+            // 
+            this.picPaddle.BackColor = System.Drawing.Color.Transparent;
+            this.picPaddle.Image = ((System.Drawing.Image)(resources.GetObject("picPaddle.Image")));
+            this.picPaddle.Location = new System.Drawing.Point(440, 520);
+            this.picPaddle.Name = "picPaddle";
+            this.picPaddle.Size = new System.Drawing.Size(120, 20);
+            this.picPaddle.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picPaddle.TabIndex = 0;
+            this.picPaddle.TabStop = false;
+            // 
+            // picBall
+            // 
+            this.picBall.BackColor = System.Drawing.Color.Transparent;
+            this.picBall.Image = ((System.Drawing.Image)(resources.GetObject("picBall.Image")));
+            this.picBall.Location = new System.Drawing.Point(488, 400);
+            this.picBall.Name = "picBall";
+            this.picBall.Size = new System.Drawing.Size(24, 24);
+            this.picBall.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBall.TabIndex = 1;
+            this.picBall.TabStop = false;
+            // 
+            // lblGameOver
+            // 
+            this.lblGameOver.AutoSize = true;
+            this.lblGameOver.BackColor = System.Drawing.Color.Transparent;
+            this.lblGameOver.Font = new System.Drawing.Font("Cambria", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGameOver.ForeColor = System.Drawing.Color.Firebrick;
+            this.lblGameOver.Location = new System.Drawing.Point(325, 240);
+            this.lblGameOver.Name = "lblGameOver";
+            this.lblGameOver.Size = new System.Drawing.Size(353, 75);
+            this.lblGameOver.TabIndex = 2;
+            this.lblGameOver.Text = "Game Over";
+            this.lblGameOver.UseMnemonic = false;
+            this.lblGameOver.Visible = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "element_blue_rectangle.png");
+            this.imageList1.Images.SetKeyName(1, "element_green_rectangle.png");
+            this.imageList1.Images.SetKeyName(2, "element_grey_rectangle.png");
+            this.imageList1.Images.SetKeyName(3, "element_purple_rectangle.png");
+            this.imageList1.Images.SetKeyName(4, "element_red_rectangle.png");
+            this.imageList1.Images.SetKeyName(5, "element_yellow_rectangle.png");
+            // 
+            // lblStart
+            // 
+            this.lblStart.AutoSize = true;
+            this.lblStart.BackColor = System.Drawing.Color.Transparent;
+            this.lblStart.Font = new System.Drawing.Font("Cambria", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStart.ForeColor = System.Drawing.Color.Orange;
+            this.lblStart.Location = new System.Drawing.Point(275, 190);
+            this.lblStart.Name = "lblStart";
+            this.lblStart.Size = new System.Drawing.Size(452, 57);
+            this.lblStart.TabIndex = 3;
+            this.lblStart.Text = "Press space to start";
+            this.lblStart.UseMnemonic = false;
             // 
             // BrickBreakerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.lblStart);
+            this.Controls.Add(this.lblGameOver);
+            this.Controls.Add(this.picBall);
+            this.Controls.Add(this.picPaddle);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Location = new System.Drawing.Point(492, 520);
             this.MaximizeBox = false;
             this.Name = "BrickBreakerForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Brick Breaker";
+            this.Load += new System.EventHandler(this.BrickBreakerForm_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.BrickBreakerForm_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.BrickBreakerForm_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.BrickBreakerForm_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.picPaddle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox picPaddle;
+        private System.Windows.Forms.PictureBox picBall;
+        private System.Windows.Forms.Label lblGameOver;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Label lblStart;
     }
 }
 
--- BrickBreakerGame/BrickBreakerForm.cs
+++ BrickBreakerGame/BrickBreakerForm.cs
@@ -12,9 +12,229 @@ namespace BrickBreakerGame
 {
     public partial class BrickBreakerForm : Form
     {
+        Timer gameTimer = new Timer();
+
+        int ballSpeed = 5;
+        int ballDX = 1;
+        int ballDY = 1;
+
+        int paddleSpeed = 10;
+        int inputDX = 0;
+
+        Image[,] Blocks;
+        int blockRows;
+        int blockCols;
+        int blockCount = 0;
+        bool gameRunning = false;
+
+        Random rand = new Random();
         public BrickBreakerForm()
         {
             InitializeComponent();
+        }
+
+        private void ShowMenu(bool show = true)
+        {
+            lblStart.Visible = show;
+            lblGameOver.Visible = show;
+            if (!show)
+            {
+                InitializeGame();
+            }
+            gameRunning = !show;
+            gameTimer.Enabled = !show;
+            Invalidate();
+        }
+
+        private int MakeBlocks(int rows, int cols)
+        {
+            Blocks = new Image[rows, cols];
+            for (int i = 0; i < rows; ++i)
+            {
+                for (int j = 0; j < cols; ++j)
+                {
+                    int index = rand.Next(0, imageList1.Images.Count);
+                    Blocks[i, j] = imageList1.Images[index];
+                }
+            }
+            return rows * cols;
+        }
+
+        private void MovePaddle(int newXPos)
+        {
+            if (newXPos < 0)
+            {
+                newXPos = 0;
+            }
+            else if (newXPos > ClientRectangle.Width - picPaddle.Width)
+            {
+                newXPos = ClientRectangle.Width - picPaddle.Width;
+            }
+            picPaddle.Left = newXPos;
+        }
+
+        private void BrickBreakerForm_Load(object sender, EventArgs e)
+        {
+            InitializeGame();
+
+            gameTimer.Interval = 16; //60fps
+            gameTimer.Tick += GameTimer_Tick;
+        }
+
+        private void InitializeGame()
+        {
+            MovePaddle((ClientRectangle.Width - picPaddle.Width) / 2);
+
+            MoveBall((ClientRectangle.Width - picBall.Width) / 2, (ClientRectangle.Height - picBall.Height) / 3 * 2);
+
+            blockRows = 7;
+            blockCols = imageList1.Images.Count * 2;
+            blockCount = MakeBlocks(blockRows, blockCols);
+        }
+
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            MovePaddle(picPaddle.Left + inputDX);
+            MoveBall(picBall.Left + ballSpeed * ballDX, picBall.Top + ballSpeed * ballDY);
+            DetectCollisionWithPaddle();
+            DetectCollisionWithBrick();
+        }
+
+        private void MoveBall(int newXPos, int newYPos)
+        {
+            Point ballPos = new Point(newXPos, newYPos);
+            picBall.Location = ballPos;
+
+            if (ballPos.X < 0 || ballPos.X > ClientRectangle.Width - picBall.Width)
+            {
+                ballDX = -ballDX;
+            }
+            if (ballPos.Y < 0)
+            {
+                ballDY = -ballDY;
+            }
+            if (ballPos.Y > ClientRectangle.Height)
+            {
+                //GAME OVER
+                ShowMenu();
+            }
+        }
+
+        private void DetectCollisionWithPaddle()
+        {
+            if (picBall.Bounds.IntersectsWith(picPaddle.Bounds))
+            {
+                ballDY = -ballDY;
+            }
+        }
+
+        private void DetectCollisionWithBrick()
+        {
+            Point ballPos = picBall.Location;
+            Point[] ballCornersPoints = new Point[]
+            {
+                new Point(ballPos.X, ballPos.Y),
+                new Point(ballPos.X + picBall.Width, ballPos.Y),
+                new Point(ballPos.X, ballPos.Y + picBall.Height),
+                new Point(ballPos.X +picBall.Width, ballPos.Y + picBall.Height),
+            };
+            int imgWidth = imageList1.ImageSize.Width;
+            int imgHeight = imageList1.ImageSize.Height;
+
+            int initialXpos = (ClientRectangle.Width - imgWidth * blockCols) / 2;
+            int initialYpos = 30;
+
+            int blockHitCount = 0;
+            foreach (Point bollCorner in ballCornersPoints)
+            {
+                int currentBrickRow = (bollCorner.Y - initialYpos) / imgHeight;
+                int currentBrickCol = (bollCorner.X - initialXpos) / imgWidth;
+                if (currentBrickCol >= 0 && currentBrickCol < blockCols && currentBrickRow >= 0 && currentBrickRow < blockRows)
+                {
+                    if (Blocks[currentBrickRow, currentBrickCol] != null)
+                    {
+                        Blocks[currentBrickRow, currentBrickCol] = null;
+                        Rectangle rectangleToInvalidate = new Rectangle()
+                        {
+                            X = initialXpos + currentBrickCol * imgWidth,
+                            Y = initialYpos + currentBrickRow * imgHeight,
+                            Width = imgWidth,
+                            Height = imgHeight
+                        };
+                        Invalidate(rectangleToInvalidate);
+                        ++blockHitCount;
+                    }
+                }
+            }
+            if (blockHitCount > 0)
+            {
+                ballDY = -ballDY;
+                blockCount -= blockHitCount;
+                if(blockCount == 0)
+                {
+                    ShowMenu();
+                }
+            }
+        }
+
+        private void BrickBreakerForm_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            if (lblStart.Visible)
+            {
+                return;
+            }
+            int imgWidth = imageList1.ImageSize.Width;
+            int imgHeight = imageList1.ImageSize.Height;
+
+            int initialXpos = (ClientRectangle.Width - imgWidth * blockCols) / 2;
+            int ypos = 30;
+
+            for (int i = 0; i < blockRows; ++i)
+            {
+                int xpos = initialXpos;
+
+                for (int j = 0; j < blockCols; ++j)
+                {
+                    if (Blocks[i, j] != null)
+                    {
+                        g.DrawImage(Blocks[i, j], xpos, ypos);
+                    }
+                    xpos += imgWidth;
+                }
+                ypos += imgHeight;
+            }
+        }
+
+        private void BrickBreakerForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Space:
+                    if (!gameRunning)
+                    {
+                        ShowMenu(false);
+                    }
+                    break;
+                case Keys.Left:
+                    if (gameRunning)
+                    {
+                        inputDX = -paddleSpeed;
+                    }
+                    break;
+                case Keys.Right:
+                    if (gameRunning)
+                    {
+                        inputDX = paddleSpeed;
+                    }
+                    break;
+            }
+        }
+
+        private void BrickBreakerForm_KeyUp(object sender, KeyEventArgs e)
+        {
+            inputDX = 0;
         }
     }
 }
